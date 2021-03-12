9a3f6788ea64e83f87df5ebdc4c91ac680afab09
Florian da Costa <florian.dacosta@akretion.com>
2021-03-12 21:50:53 +0100

    Migrate synchronizer to v14

---
+++ synchronizer/__manifest__.py
@@ -0,0 +1,15 @@
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+{
+    "name": "Synchronizer",
+    "version": "14.0.1.0.0",
+    "category": "Generic Modules",
+    "description": """
+    Module that extends Odoo's ORM to add extra features for synchronisations
+    """,
+    "author": "Akretion",
+    "website": "http://akretion.com",
+    "depends": ["base"],
+    "installable": True,
+    "application": False,
+}
---
+++ synchronizer/models/__init__.py
@@ -0,0 +1,1 @@
+from . import synchronized_mixin
